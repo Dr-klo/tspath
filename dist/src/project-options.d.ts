e7c123c99b8c9c64da1b8e6776150344193bc37c
Dr.klo <leonid.kulinchik@gotechsoftware.com>
2019-04-29 17:03:57 +0200

    fix compilation

--- dist/src/project-options.d.ts
+++
@@ -1,9 +0,0 @@
-import { ISettings } from "./type-definitions";
-export declare class ProjectOptions {
-    outDir: string;
-    baseUrl: string;
-    rootDirs: string[];
-    pathMappings: ISettings;
-    processMappings(mappings: any): void;
-    constructor(tsconfigObj: any);
-}
